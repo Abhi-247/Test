80f1abc61abeb9324f1ce5b74641e92d5ca8ffe3
Abhishek verma <mrabhiverma1895@gmail.com>
2026-03-06 10:53:01 +0530

    Initialized

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* global __BUILD_DATE__ */
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      {/* This will show the date from the file in BOTH dev and prod */}
+      
+
+<h1>Version 1.0 | Build {__BUILD_DATE__}</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ command }) => {
+
+  const buildDate =
+    command === "build"
+      ? new Date().toISOString().slice(0,10).replace(/-/g,'')
+      : "20250901"; // previous build date
+
+  return {
+    plugins: [react()],
+    define: {
+      __BUILD_DATE__: JSON.stringify(buildDate),
+    },
+  };
+});
